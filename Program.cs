5aaa582ce4cc543f71f26af06149f95fde8f4261
JasmileR <97096907+JasmileR@users.noreply.github.com>
2026-02-18 13:51:05 +0000

    Add EF Core migrations setup and update Redis connection handling

--- Program.cs
+++ Program.cs
@@ -9,16 +9,16 @@ if(builder.Environment.IsDevelopment())
         options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
     builder.Services.AddDistributedMemoryCache();
 }
-// else
-// {
-//     builder.Services.AddDbContext<MyDatabaseContext>(options =>
-//         options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
-//     builder.Services.AddStackExchangeRedisCache(options =>
-//     {
-//     options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
-//     options.InstanceName = "SampleInstance";
-//     });
-// }
+else
+{
+    builder.Services.AddDbContext<MyDatabaseContext>(options =>
+        options.UseSqlServer("Database=msdocs-core-sql-database;Server=msdocs-core-mysql-server.mysql.database.azure.com;User Id=lzehdiomtm;Password=2tYJq$4oLMyzW2$Z"));
+    builder.Services.AddStackExchangeRedisCache(options =>
+    {
+    options.Configuration = builder.Configuration["REDIS_CONNECTION_STRING"];
+    options.InstanceName = "SampleInstance";
+    });
+}
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
